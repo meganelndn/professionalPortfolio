08cad6ceb509bd64cf87221b7b29aad2a03f81d7
megane <megane.londono@hotmail.com>
2020-04-01 14:34:18 +0200

    landing page layout

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+    let response = await fetch("imgs/logo.svg");
+    let svgData = await response.text();
+    document.querySelector(".logo").innerHTML = svgData;
+
+    let response2 = await fetch("imgs/two_girls.svg");
+    let svgData2 = await response2.text();
+    document.querySelector(".illustration-me").innerHTML = svgData2;
+
+    /* let response3 = await fetch("imgs/scroll_lines.svg");
+    let svgData3 = await response3.text();
+    document.querySelector(".line").innerHTML = svgData3; */
+}
